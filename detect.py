01b51a795b3420a2ac274e09a6598aa56125fff7
Junkyu Lim <201602057@cs-cnu.org>
2021-01-22 16:38:07 +0900

    update output type / txt -> json

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 from pathlib import Path
+import json
 
 import cv2
 import torch
@@ -94,21 +95,15 @@ def detect(save_img=False):
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
+                dic = {"With_Mask":0,"Without_Mask":0,"Incorrect_Mask":0}
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f'{n} {names[int(c)]}s, '  # add to string
+                    dic[names[int(c)]] = int(n)
 
                 # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-#                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                with open(txt_path + '.json', 'a') as f:
+                    json.dump(dic, f)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
